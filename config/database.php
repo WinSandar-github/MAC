7c23eee51e44e882bf4c08f401ce13c0bb951d11
Khun Noon <khunzayar@agga.io>
2021-03-30 09:51:33 +0630

    connect with moodle database

--- config/database.php
+++ config/database.php
@@ -48,8 +48,8 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'tms'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
@@ -61,6 +61,16 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+        ],
+
+        'mysql2' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL_SECOND'),
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'moodle',
+            'username' => 'root',
+            'password' => '',
         ],
 
         'pgsql' => [
