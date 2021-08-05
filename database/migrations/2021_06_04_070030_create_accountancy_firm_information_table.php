4ae306ddb5446ccea1181864673931b3aeedd7b3
chu yamone lwin <chuyamonelwin@agga.io>
2021-08-05 21:06:21 +0630

    fix accountancy_firm_information table migration and download cdn links into project folder

--- database/migrations/2021_06_04_070030_create_accountancy_firm_information_table.php
+++ database/migrations/2021_06_04_070030_create_accountancy_firm_information_table.php
@@ -16,7 +16,8 @@ class CreateAccountancyFirmInformationTable extends Migration
         Schema::create('accountancy_firm_information', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('audit_firm_type_id');
-            $table->unsignedBigInteger('local_foreign_id');
+            //$table->unsignedBigInteger('local_foreign_id');
+            $table->unsignedBigInteger('local_foreign_type')->nullable();
             $table->string('accountancy_firm_reg_no');
             $table->string('accountancy_firm_name');
             $table->string('township');
@@ -42,10 +43,10 @@ class CreateAccountancyFirmInformationTable extends Migration
             ->on('audit_firm_types')
             ->onDelete('cascade');
 
-            $table->foreign('local_foreign_id')
-            ->references('id')
-            ->on('local_foreigns')
-            ->onDelete('cascade');
+            // $table->foreign('local_foreign_id')
+            // ->references('id')
+            // ->on('local_foreigns')
+            // ->onDelete('cascade');
 
             $table->foreign('type_of_service_provided_id')
             ->references('id')
