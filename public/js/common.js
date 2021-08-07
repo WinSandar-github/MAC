7eaf32729a4fc4356601ac0734fe225caeda6ad0
nyeinsandar <nyeinsandar@agga.io>
2021-08-07 20:11:54 +0630

    fixed bugs

--- public/js/common.js
+++ public/js/common.js
@@ -1,9 +1,9 @@
-// var BACKEND_URL="http://localhost:8000/api";
-// var FRONTEND_URL="http://localhost:8000";
-//  var PDF_URL="http://localhost:8000";
-var BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
-var FRONTEND_URL = "https://demo.aggademo.me/MAC/public/index.php";
-var PDF_URL =  "https://demo.aggademo.me/MAC/public";
+var BACKEND_URL="http://localhost:8000/api";
+var FRONTEND_URL="http://localhost:8000";
+ var PDF_URL="http://localhost:8000";
+// var BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
+// var FRONTEND_URL = "https://demo.aggademo.me/MAC/public/index.php";
+// var PDF_URL =  "https://demo.aggademo.me/MAC/public";
 var counter = 0;
 
 function addRowCPAFF(tbody){
