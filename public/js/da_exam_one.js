20e6dc4322044094f2fabe4ec46ab144ebaa0d59
Si Thu <sithuaung182@gmail.com>
2021-08-10 21:25:03 +0630

    add pass or fail in result list

--- public/js/da_exam_one.js
+++ public/js/da_exam_one.js
@@ -361,37 +361,38 @@ function loadStudent(course_type)
             console.log("course",data);
             var da_data = data.data;
             da_data.forEach(function (element) {
-                if(element.exam_register!=null){    
+             
+                    console.log(element)
                     $.ajax({
-                        url: BACKEND_URL+"/course/"+element.exam_register.form_type,
+                        url: BACKEND_URL+"/course/"+element.form_type,
                         type: 'get',
                         data:"",
                         success:function(courses){
                             var course =courses.data;
                             if(course[0].code==course_type){
                                 console.log("stu_reg",course_type);
-                                if(element.exam_register.status==0){
+                                if(element.status==0){
                                     status="PENDING";
                                 }
-                                else if(element.exam_register.status==1){
+                                else if(element.status==1){
                                     status="APPROVED";
                                 }
                                 else{
                                     status="REJECTED";
                                 }
-                                if(element.exam_register.exam_type_id == 0){
+                                if(element.exam_type_id == 0){
                                     exam_type_id = "SELF STUDY";
                                 }
-                                else if(element.exam_register.exam_type_id==1){
+                                else if(element.exam_type_id==1){
                                     exam_type_id="PRIVATE SCHOOL";
                                 }
                                 else{
                                     exam_type_id="MAC STUDENT";
                                 }
-                                if(element.exam_register.is_full_module==0){
+                                if(element.is_full_module==0){
                                     is_full_module="Module 1";
                                 }
-                                else if(element.exam_register.is_full_module==1){
+                                else if(element.is_full_module==1){
                                     is_full_module="Module 2";
                                 }
                                 else{
@@ -400,15 +401,15 @@ function loadStudent(course_type)
                 
                                 var tr = "<tr>";
                                 tr += "<td>" +  + "</td>";
-                                tr += "<td>" + element.name_eng + "</td>";
-                                tr += "<td>" + element.exam_register.private_school_name + "</td>";
+                                tr += "<td>" + element.student_info.name_eng + "</td>";
+                                tr += "<td>" + element.private_school_name + "</td>";
                                 tr += "<td>" + exam_type_id + "</td>";
-                                tr += "<td>" + element.exam_register.grade + "</td>";
+                                tr += "<td>" + element.grade + "</td>";
                                 tr += "<td>" + status+ "</td>";
-                                tr += "<td>" + element.exam_register.batch_id+ "</td>";
+                                tr += "<td>" + element.batch_id+ "</td>";
                                 tr += "<td>" + is_full_module+ "</td>";
                                 tr += "<td ><div class='btn-group'>";
-                                tr+="<button type='button' class='btn btn-primary btn-xs' onClick='fillMark(" + element.exam_register.id + "," + element.exam_register.is_full_module +")'>" +
+                                tr+="<button type='button' class='btn btn-primary btn-xs' onClick='fillMark(" + element.id + "," + element.is_full_module +")'>" +
                                     "<li class='fa fa-eye fa-sm'></li></button></div ></td > ";
                                 tr += "<td ><div class='btn-group'>";
                                 $("#tbl_exam_result_body").append(tr);
@@ -419,7 +420,7 @@ function loadStudent(course_type)
                             }
                         }
                     });
-                }         
+                // }        
                     
                 //});
                 
