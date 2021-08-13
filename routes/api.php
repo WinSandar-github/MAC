91988e15cbf38f1eee4cbee61c48ab4e15a7aeee
naylin <naylin@securelinkmm.com>
2021-08-13 19:00:44 +0630

    edit mentor

--- routes/api.php
+++ routes/api.php
@@ -173,8 +173,8 @@ Route::get('/get_exam_status/{id}','ExamRegisterController@getExamStatus');
 Route::patch('/pass_exam/{id}', 'ExamResultController@passExam');
 Route::patch('/fail_exam/{id}', 'ExamResultController@failExam');
 
-// Route::apiResource('mentor','MentorController');
-Route::resource('mentor','MentorController');
+Route::apiResource('mentor','MentorController');
+// Route::resource('mentor','MentorController');
 Route::get('check_service','CurrentCheckServiceController@getCurrentCheckService');
 
 
