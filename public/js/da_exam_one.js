57d34c052e825401a51222e681ddc776df8ead76
laywahshee <laywahshee@agga.io>
2021-08-19 02:43:27 +0630

    fix bugs

--- public/js/da_exam_one.js
+++ public/js/da_exam_one.js
@@ -45,7 +45,7 @@ function getExam(){
                                 var tr = "<tr>";
                                 tr += "<td>" +  + "</td>";
                                 tr += "<td>" + element.student_info.name_eng + "</td>";
-                                tr += "<td>" + element.private_school_name + "</td>";
+                                //tr += "<td>" + element.private_school_name + "</td>";
                                 tr += "<td>" + exam_type_id + "</td>";
                                 tr += "<td>" + element.grade + "</td>";
                                 tr += "<td>" + status+ "</td>";
