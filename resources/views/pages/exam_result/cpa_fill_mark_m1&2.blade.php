8800db03fd69fef4e5e962a2b7009da077f513d0
Si Thu <sithuaung182@gmail.com>
2021-08-23 12:32:15 +0630

    change ui mac student dashboard

--- resources/views/pages/exam_result/cpa_fill_mark_m1&2.blade.php
+++ resources/views/pages/exam_result/cpa_fill_mark_m1&2.blade.php
@@ -150,8 +150,8 @@
                                             </form>
                                                 <div class="pass_fail_btn" style="display:none;">
 
-                                                    <button  class=" btn btn-primary" onClick="javascript:passExam();">Pass</button>
                                                     <button class=" btn  btn-danger" onClick="javascript:failExam();">Fail</button>
+                                                    <button  class=" btn btn-primary" onClick="javascript:passExam();">Pass</button>
                                                 </div>
                                             </div>
                                         </div>
