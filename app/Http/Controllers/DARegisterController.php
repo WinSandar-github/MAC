4a7559530430b803e42a5d882de5a4cc855ca59f
nyeinsandar <nyeinsandar@agga.io>
2021-09-01 13:08:46 +0630

    fixed sidebar

--- app/Http/Controllers/DARegisterController.php
+++ app/Http/Controllers/DARegisterController.php
@@ -281,12 +281,9 @@ class DARegisterController extends Controller
          
          $student_register = StudentRegister::where('student_info_id',$id)->where('form_type',$stu_course_reg->batch->course_id)->first();
          $status = $student_register != null ? $student_register->status : null;
-<<<<<<< HEAD
-         // print_r($stu_course_reg);
-        return response()->json($stu_course_reg,200);
-=======
+        // print_r($stu_course_reg);
+        // return response()->json($stu_course_reg,200);
          return response()->json($status,200);
->>>>>>> 78d05fa9d4ce00187c60b12cb19f03d334efb987
 
     }
 
