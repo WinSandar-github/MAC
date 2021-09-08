f32c23a2828150021333c6e28b1ec8cd5b984ce3
San Lai Mon <sanlaimon@agga.io>
2021-09-08 14:08:33 +0630

    reporting

--- resources/views/layouts/navbars/auth.blade.php
+++ resources/views/layouts/navbars/auth.blade.php
@@ -250,11 +250,11 @@
                     <b class="caret"></b>
                 </a>
             </li>
-            <li class="">
-                <a href="#">
-                    <i class="nc-icon nc-tap-01"></i>
-                    <p>{{ __('Reporting') }}</p>
-                    <b class="caret"></b>
+            <li class="{{ $elementActive == 'reporting_list' ? 'active' : '' }}">
+                <a href="{{ route('page.index', 'reporting_list') }}">
+                    <i class="nc-icon nc-paper"></i>
+                    <span>{{ __('Reporting') }}</span>
+                    <!-- <b class="caret"></b> -->
                 </a>
             </li>
 
