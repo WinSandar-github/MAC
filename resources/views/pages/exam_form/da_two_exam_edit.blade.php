b4c6cca0e0ac54416a431dac4f71456606b2824c
chu yamone lwin <chuyamonelwin@agga.io>
2021-09-15 01:57:17 +0630

    add exam department and show information in exam register lists

--- resources/views/pages/exam_form/da_two_exam_edit.blade.php
+++ resources/views/pages/exam_form/da_two_exam_edit.blade.php
@@ -9,84 +9,246 @@
     <div class="content">
         @include('flash-message')
         <div class="row">
-            <div class="col-md-12">   
-                   
+            <div class="col-md-12">
+
             </div>
-        </div>       
+        </div>
 
         <div class="row">
             <div class="col-md-12 text-center">
                 <form action="javascript:void()" method="post" enctype="multipart/form-data">
                     @csrf
                     <div class="card">
-                        
+
                         <div class="card-body">
                             <div class="row">
-                                
-                                <div class="col-md-12">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h3>Basic Info
-                                        </div>
-                                        <div class="card-body">
-                                            <div class="row">
-                                                <label class="col-md-2 form-label"></label>
-                                                <label class="col-md-3 form-label text-left">{{ __('Private School Name') }}</label>
-                                                <label class="col-md-1 form-label"></label>
-                                                <div class="col-md-5 text-left">
-                                                    <div class="form-group">
-                                                        <span id="school_name"></span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row">
-                                                <label class="col-md-2 form-label"></label>
-                                                <label class="col-md-3 form-label text-left">{{ __('Exam Type') }}</label>
-                                                <label class="col-md-1 form-label"></label>
-                                                <div class="col-md-5 text-left">
-                                                    <div class="form-group">
-                                                        <span id="exam_type"></span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row">
-                                                <label class="col-md-2 form-label"></label>
-                                                <label class="col-md-3 form-label text-left">{{ __('Grade') }}</label>
-                                                <label class="col-md-1 form-label"></label>
-                                                <div class="col-md-5 text-left">
-                                                    <div class="form-group">
-                                                        <span id="student_grade"></span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row">
-                                                <label class="col-md-2 form-label"></label>
-                                                <label class="col-md-3 form-label text-left">{{ __('Status') }}</label>
-                                                <label class="col-md-1 form-label"></label>
-                                                <div class="col-md-5 text-left">
-                                                    <div class="form-group">
-                                                        <span id="student_status"></span>
-                                                    </div>
-                                                </div>
-                                            </div>
+                              <div class="col-md-4 ">
+                                  <center>
+                                      <img id="image" width="30%" class="rounded-circle" style="width: 100px;height : 100px" />
+                                  </center>
+                                  <h5 class="border-bottom pb-2 mt-3"  style="font-weight:bold">Education</h5>
+                                  <div class="row m-2 mt-3 border-bottom">
+                                      <div class="col-md-6">
+                                          <p class="ml-2" style="font-weight:bold">Universtry Name</p>
+                                      </div>
+                                      <div class="col-md-6">
+                                          <span id="university_name"></span>
+                                      </div>
+                                  </div>
+                                  <div class="row m-2 mt-3 border-bottom">
+                                      <div class="col-md-6">
+                                          <p class="ml-2"  style="font-weight:bold">Degree Name</p>
+                                      </div>
+                                      <div class="col-md-6">
+                                          <span id="degree_name"></span>
+                                      </div>
+                                  </div>
+                                  <div class="row m-2 mt-3 border-bottom">
+                                      <div class="col-md-6">
+                                          <p class="ml-2" style="font-weight:bold">Qualified Date</p>
+                                      </div>
+                                      <div class="col-md-6">
+                                          <span id="qualified_date"></span>
+                                      </div>
+                                  </div>
+                                  <div class="row m-2 mt-3 border-bottom">
+                                      <div class="col-md-6">
+                                          <p class="ml-2" style="font-weight:bold">Roll Number</p>
+                                      </div>
+                                      <div class="col-md-6">
+                                          <span id="roll_number"></span>
+                                      </div>
+                                  </div>
+                                  <div class="row m-2 mt-3 border-bottom">
+                                      <div class="col-md-6">
+                                          <p class="ml-2" style="font-weight:bold">Attached Certificate</p>
+                                      </div>
+                                      <div class="col-md-6 certificate">
 
-                                            <input type="hidden" name="student_id">
-                                            
-                                            <div class="row mt-5 justify-content-center">                                                 
-                                                <button type="submit"  id="reject" name="save" class="btn btn-danger"  onclick="rejectDATwoExam()" style="width : 20%"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i>REJECT</button>
-                                                <button type="submit" id="approve" name="save" class="btn btn-primary" onclick="approveDATwoExam()" style="width : 20%"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>APPROVE</button>
+                                          <!-- <button type="button" style="width: 30%;margin-top:1% ;" class="btn btn-primary" data-toggle="modal" data-target="#myModal"><i class="fa fa-paperclip"></i></button> -->
+                                          <!-- <button type="button" style="width: 30%;margin-top:1% ;" class="btn btn-primary" onclick="file_read('certificate')"><i class="fa fa-paperclip"></i></button> -->
+                                      </div>
+                                  </div>
 
-                                            </div>
+                                  <h5 class="border-bottom pb-2 mt-3"  style="font-weight:bold">Exam</h5>
+
+                                  <input type="hidden" name="student_id">
+
+                                  <div class="row m-2 mt-3 border-bottom">
+                                      <div class="col-md-6">
+                                          <p class="ml-2" style="font-weight:bold">Private School Name</p>
+                                      </div>
+                                      <div class="col-md-6">
+                                          <span id="school_name"></span>
+                                      </div>
+                                  </div>
+
+                                  <div class="row m-2 mt-3 border-bottom">
+                                      <div class="col-md-6">
+                                          <p class="ml-2" style="font-weight:bold">Exam Type</p>
+                                      </div>
+                                      <div class="col-md-6">
+                                          <span id="exam_type"></span>
+                                      </div>
+                                  </div>
+
+                                  <div class="row m-2 mt-3 border-bottom">
+                                      <div class="col-md-6">
+                                          <p class="ml-2" style="font-weight:bold">Grade</p>
+                                      </div>
+                                      <div class="col-md-6">
+                                          <span id="student_grade"></span>
+                                      </div>
+                                  </div>
+
+                                  <div class="row m-2 mt-3 border-bottom">
+                                      <div class="col-md-6">
+                                          <p class="ml-2" style="font-weight:bold">Status</p>
+                                      </div>
+                                      <div class="col-md-6">
+                                          <span id="student_status"></span>
+                                      </div>
+                                  </div>
+
+                                  <div class="row m-2 mt-3 border-bottom">
+                                      <div class="col-md-6">
+                                          <p class="ml-2" style="font-weight:bold">စာဖြေဌာန</p>
+                                      </div>
+                                      <div class="col-md-6">
+                                          <span id="exam_department"></span>
+                                      </div>
+                                  </div>
+                              </div>
+
+                                <input type="hidden" name="student_id">
+
+                                <div class="col-md-8">
+                                    <h5 class="border-bottom pb-2" style="font-weight:bold">Student Information</h5>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2 text-bold" style="font-weight:bold">Name(Eng) / Name(Myanmar)</p>
                                         </div>
-                                            
+                                        <div class="col-md-6">
+                                            <span id="name_eng"></span> / <span id="name_mm"></span>
                                         </div>
                                     </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">NRC</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="nrc"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">Father Name(Eng) / Father Name(Myanmar)</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="father_name_mm"></span> / <span id="father_name_eng"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">Race</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="race"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">Religion</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="religion"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">Date of Birth</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="date_of_birth"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">Address</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="address"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">Current Address</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="current_address"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">Phone</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="phone"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">Email</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="email"></span>
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">Government Staff</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="gov_staff"></span>
+                                            {{--<p>{{ $user->gov_staff == 0 ? 'No' : 'Yes'}}</p>--}}
+                                        </div>
+                                    </div>
+                                    <div class="row m-2 mt-3 border-bottom">
+                                        <div class="col-md-6">
+                                            <p class="ml-2" style="font-weight:bold">ကိုယ်ပိုင်အမှတ်</p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <span id="registration_no"></span>
+                                        </div>
+                                    </div>
+
+                                    <input type="hidden" name="student_course_id" >
+
+
+
+                                    <div class="row mt-5 justify-content-center">
+                                        <button type="submit"  id="reject" name="save" class="btn btn-danger"  onclick="rejectDATwoExam()" style="width : 20%"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i>REJECT</button>
+                                        <button type="submit" id="approve" name="save" class="btn btn-primary" onclick="approveDATwoExam()" style="width : 20%"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>APPROVE</button>
+
+                                    </div>
+                                    <!-- Attached Certificate -->
+                                    <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" >
+                                      <div class="modal-dialog modal-lg" role="document" >
+                                        <div class="modal-content">
+                                            <div class="modal-body">
+                                                <embed id="attach_file"  width="700px" height="500px">
+                                            </div>
+                                        </div>
+                                      </div>
+                                    </div>
                                 </div>
+
                             </div>
-                                            
                         </div>
+
                     </div>
-                </form>
+                </div>
+              </form>
             </div>
         </div>
     </div>
