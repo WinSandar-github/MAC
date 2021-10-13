f4c880ce31b4615b8b36f009a5e2bbc7239022f9
sandar <sandar@agga.io>
2021-10-13 23:50:46 +0630

    update profile teacher and school

--- routes/api.php
+++ routes/api.php
@@ -421,7 +421,3 @@ Route::post('/renewSchool', 'SchoolController\SchoolController@renewSchool');
 Route::get('/getSchoolInfo/{id}', 'SchoolController\SchoolController@getSchoolInfo');
 Route::patch('/renewSchoolPayment', 'SchoolController\SchoolController@renewSchoolPayment');
 Route::post('/renewUpdateSchool/{id}', 'SchoolController\SchoolController@renewUpdateSchool');
-//teacher profile
-Route::patch('updateProfileTeacher/{id}','TeacherController\TeacherController@updateProfileTeacher');
-//school profile
-Route::patch('updateProfileSchool/{id}','SchoolController\SchoolController@updateProfileSchool');
