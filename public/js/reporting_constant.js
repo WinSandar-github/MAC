05fee7f26148fe526fcb3dd3ccbe570a12670728
DESKTOP-BKLVCMT\Twetar <twetar>
2021-10-15 17:26:39 +0630

    fixed reporting ui.

--- public/js/reporting_constant.js
+++ public/js/reporting_constant.js
@@ -67,3 +67,85 @@ const _CPA = [
     // MODULE (၂)ခုအနက် (၁)ခုအောင်မြင်မှု မရှိသေးသူများစာရင်း၊ 
     // တဆက်တည်း စာမေးပွဲဝင်ရောက်ဖြေဆိုမှုမရှိသူများစာရင်း (ကျောင်းသားအမျိုးအစားအလိုက်၊ စာဖြေဌာနအလိုက်၊ MODULEအလိုက်၊ ကျောင်းအလိုက်)`
 ]
+
+const _CPA_QUALIFIED = [
+    { 
+        route_name: "/cpa_qualified_enrol",
+        // fun_name : 'cpaQualifiedList($(this).data(\'url\'))',
+        sub_title:"လျှောက်ထားသူများစာရင်း (စာမေးပွဲကျင်းပသည့် ခုနှစ်နှင့်လအလိုက်)" 
+    },
+
+    {
+        route_name: "ca_qualified_exam_enrol",
+        // fun_name : 'cpaQualifiedList($(this).data(\'url\'))',
+        sub_title: "ဖြေဆိုခွင့်ရရှိသူများစာရင်း (စာမေးပွဲကျင်းပသည့် ခုနှစ်နှင့်လအလိုက်)"
+    },
+
+    {
+        route_name: "/ca_qualified_exam_reg",
+        // fun_name : 'cpaQualifiedList($(this).data(\'url\'))',
+        sub_title: "ဖြေဆိုသူများစာရင်း (စာမေးပွဲကျင်းပသည့် ခုနှစ်နှင့်လအလိုက်)"
+    },
+
+    {
+        route_name: "/ca_qualified_pass",
+        // fun_name : 'cpaQualifiedList($(this).data(\'url\'))',
+        sub_title: "အောင်မြင်သူများစာရင်း (စာမေးပွဲကျင်းပသည့် ခုနှစ်နှင့်လအလိုက်)"
+    },
+
+    {
+        route_name: "/ca_qualified_fail",
+        // fun_name : 'cpaQualifiedList($(this).data(\'url\'))',
+        sub_title: "ကျရှံးသူများစာရင်း (စာမေးပွဲကျင်းပသည့် ခုနှစ်နှင့်လအလိုက်)"
+    }
+    
+]
+
+const _CPA_PAPP = [
+    { 
+        route_name: "/cpa_papp_report_template",
+        // fun_name : 'cpaPAPPList($(this).data(\'url\'))',
+        sub_title: "CPA (FF)/ PA တစ်ဦး၏ သက်တမ်းတိုးမည့် ပြက္ဒဒိန်နှစ်အပါအ၀င် ကပ်လျက်ရှိသော ၂နှစ်၏ CPD နာရီမှတ်တမ်း" 
+    },
+
+    {
+        route_name: "/cpa_papp_report_template",
+        // fun_name : 'cpaPAPPList($(this).data(\'url\'))',
+        sub_title: "ပြက္ဒဒိန်နှစ် အလိုက် မှတ်ပုံတင်လုပ်ငန်းများစာရင်း"
+    },
+
+    {
+        
+        route_name: "/cpa_papp_report_template",
+        // fun_name : 'cpaPAPPList($(this).data(\'url\'))',
+        sub_title: "မှတ်ပုံတင်ကတ်ပြားများကို စနစ်ဖြင့် ထုတ်ယူခြင်း"
+    }
+    
+]
+
+const _ARTICLE = [
+    { 
+        route_name: "/article_report_template",
+        // fun_name : 'articleList($(this).data(\'url\'))',
+        sub_title: "အလုပ်သင်ကြားပေးနိုင်သည့် အများပြည်သူသို့ စာရင်းဝန်ဆောင်မှု ပေးသူတစ်ဦးချင်း" 
+    },
+
+    {
+        route_name: "/article_report_template",
+        // fun_name : 'articleList($(this).data(\'url\'))',
+        sub_title: "နေ့စဥ်ရုံးတက်ရုံးဆင်းမှတ်တမ်း၊ ခွင့်ပုံစံ။"
+    },
+
+    { 
+        route_name: "/article_report_template",
+        // fun_name : 'articleList($(this).data(\'url\'))',
+        sub_title: "စာရင်းကိုင်အလုက်သင်ခန့်အပ်စာရင်း (Batch အလိုက်)"
+    },
+
+    { 
+        route_name: "/article_report_template",
+        // fun_name : 'articleList($(this).data(\'url\'))',
+        sub_title: "အလုပ်သင်ဆင်းသူများစာရင်း (Batch အလိုက်)"
+    }
+    
+]
