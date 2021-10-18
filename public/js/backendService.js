9e508e83fb47574202c172c9a59d9cc4b18e2dc4
sandar <sandar@agga.io>
2021-10-18 12:18:16 +0630

    adding

--- public/js/backendService.js
+++ public/js/backendService.js
@@ -1,7 +1,7 @@
-// const BACKEND_URL = "http://localhost:8000/api";
-// const FRONTEND_URL = "http://localhost:8000";
-// const PDF_URL = "http://localhost:8000";
+const BACKEND_URL = "http://localhost:8000/api";
+const FRONTEND_URL = "http://localhost:8000";
+const PDF_URL = "http://localhost:8000";
 
-const BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
-const FRONTEND_URL = "https://demo.aggademo.me/MAC/public/index.php";
-const PDF_URL = "https://demo.aggademo.me/MAC/public";
+// const BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
+// const FRONTEND_URL = "https://demo.aggademo.me/MAC/public/index.php";
+// const PDF_URL = "https://demo.aggademo.me/MAC/public";
