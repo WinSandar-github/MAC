1e7449b5792aecb9c2e67eb72275a408fbe9c752
nyeinsandar <nyeinsandar@agga.io>
2021-10-18 12:23:25 +0630

    adding

--- public/js/common.js
+++ public/js/common.js
@@ -1,13 +1,4 @@
-// var BACKEND_URL="http://localhost:9090/api";
-// var FRONTEND_URL="http://localhost:9090";
-// var PDF_URL="http://localhost:9090";
-
-var BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
-var FRONTEND_URL = "https://demo.aggademo.me/MAC/public/index.php";
-var PDF_URL = "https://demo.aggademo.me/MAC/public";
-
 var counter = 0;
-
 
 function ConfirmSubmit() {
     var radio = document.getElementById("submit_confirm");
