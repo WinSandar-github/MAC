e4d933400644ebc314d2d1e2a730e98e6f1c4a48
nyeinsandar <nyeinsandar@agga.io>
2021-10-22 08:26:01 +0630

    existing user for da

--- app/Http/Controllers/CpaTraAddmissionDirectController.php
+++ app/Http/Controllers/CpaTraAddmissionDirectController.php
@@ -305,8 +305,8 @@ class CpaTraAddmissionDirectController extends Controller
         $invoice->phone           = $request->phone;
 
         $std = StudentCourseReg::with('batch')->where("student_info_id", $student_info->id)->latest()->first();
-        $invoice->productDesc     = 'Application Fee, Transaction Fee,' . $std->batch->course->name;
-        $invoice->amount          = $std->batch->course->form_fee. ',1000';
+        $invoice->productDesc     = 'Application Fee, ' . $std->batch->course->name;
+        $invoice->amount          = $std->batch->course->form_fee;
         $invoice->status          = 0;
         $invoice->save();
         
