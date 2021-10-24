cd36fe1bf5567c1c42a26c9d58d5539ab225bd9e
Administrator <admin@example.com>
2021-10-24 11:54:01 +0630

    update

--- public/js/common.js
+++ public/js/common.js
@@ -1,10 +1,10 @@
-//  var BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
-//  var FRONTEND_URL = "https://demo.aggademo.me/MAC/public/index.php";
-//  var PDF_URL = "https://demo.aggademo.me/MAC/public";
+ var BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
+ var FRONTEND_URL = "https://demo.aggademo.me/MAC/public/index.php";
+ var PDF_URL = "https://demo.aggademo.me/MAC/public";
 
-var BACKEND_URL = "http://localhost:8080/api";
-var FRONTEND_URL = "http://localhost:8080";
-var PDF_URL = "http://localhost:8080";
+// var BACKEND_URL = "http://localhost:8080/api";
+// var FRONTEND_URL = "http://localhost:8080";
+// var PDF_URL = "http://localhost:8080";
 // var BASE_URL = "http://localhost:8000";
 
 
