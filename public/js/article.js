22e1caa01ff3c8672d7bb8310885ba097cc8e65a
chu yamone lwin <chuyamonelwin@agga.io>
2021-11-10 01:25:49 +0630

    fix payment show

--- public/js/article.js
+++ public/js/article.js
@@ -682,7 +682,6 @@ function showGovContractDate(info) {
 
 function showPaymentInfo(info) {
     $("#payment_detail_modal").modal('show');
-
     autoLoadPayment(info.id);
 }
 
@@ -1064,6 +1063,48 @@ function autoLoadPaymentFirm(firm_id,form_type){
                     `);
         }
     });
+}
+
+function autoLoadPayment(gov_id){
+  $("#payment_detail_modal").find(".fee_list").html('');
+  $.ajax({
+      url: BACKEND_URL + "/get_payment_info/" + 'gov'+gov_id,
+      type: 'get',
+      success: function (result) {
+
+          if(result.status==0){
+              $('#payment_status').append("Incomplete");
+              $('#payment_status').addClass("text-warning");
+          }
+          else{
+              $('#payment_status').append("Complete");
+              $('#payment_status').addClass("text-success");
+          }
+          var productDesc = result.productDesc.split(",");
+          var amount = result.amount.split(",");
+          var total=0;
+          for(var i in amount) {
+              total += parseInt(amount[i]);
+          }
+          console.log(total);
+          for(let i=0 ; i<amount.length ; i++){
+              $('.fee_list').append(`
+                  <li class="list-group-item d-flex justify-content-between lh-condensed">
+                      <h6 class="my-0">${productDesc[i]}</h6>
+                      <span class="text-muted">- ${amount[i]} MMK</span>
+                  </li>
+              `);
+          }
+          $('.fee_list').append(`
+              <li class= "list-group-item d-flex justify-content-between" >
+                  <span>Total (MMK)</span>
+                  <span id="total">
+                      - <strong>${total}</strong> MMK
+                  </span>
+              </li>
+              `);
+      }
+  });
 }
 
 function approveGovArticle() {
