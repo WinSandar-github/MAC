60b4e00fc377f009d76a90ec7b57910f2c31760b
nyeinsandar <nyeinsandar@agga.io>
2021-11-09 17:10:00 +0630

    da/cpa existing user

--- public/js/common.js
+++ public/js/common.js
@@ -1,11 +1,11 @@
-// var BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
-// var FRONTEND_URL = "https://demo.aggademo.me/MAC/public/index.php";
-// var PDF_URL = "https://demo.aggademo.me/MAC/public";
+var BACKEND_URL = "https://demo.aggademo.me/MAC/public/index.php/api";
+var FRONTEND_URL = "https://demo.aggademo.me/MAC/public/index.php";
+var PDF_URL = "https://demo.aggademo.me/MAC/public";
 
-var BACKEND_URL = "http://localhost:8000/api"; 
-var FRONTEND_URL = "http://localhost:8000";
-var PDF_URL = "http://localhost:8000";
-var BASE_URL = "http://localhost:8000"; 
+// var BACKEND_URL = "http://localhost:8000/api"; 
+// var FRONTEND_URL = "http://localhost:8000";
+// var PDF_URL = "http://localhost:8000";
+// var BASE_URL = "http://localhost:8000"; 
 
 
 var counter = 0;
