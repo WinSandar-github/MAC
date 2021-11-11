52cd9c7691f84eab0e16deb333e72a95dd27b983
chu yamone lwin <chuyamonelwin@agga.io>
2021-11-11 16:52:15 +0630

    fix feedbacks

--- resources/views/pages/article/article_list.blade.php
+++ resources/views/pages/article/article_list.blade.php
@@ -231,6 +231,9 @@
                                                             <th class="bold-font-weight" >Resign Date</th>
                                                             <th class="bold-font-weight" >Net Experience</th>
                                                             <th class="bold-font-weight" >Mentor Name</th>
+                                                            <th class="bold-font-weight" >Contract Start Date</th>
+                                                            <th class="bold-font-weight" >Contract End Date</th>
+                                                            <th class="bold-font-weight" >Form Type</th>
                                                             <th class="bold-font-weight" >Status</th>
                                                         </tr>
                                                     </thead>
@@ -252,6 +255,9 @@
                                                             <th class="bold-font-weight" >Resign Date</th>
                                                             <th class="bold-font-weight" >Net Experience</th>
                                                             <th class="bold-font-weight" >Mentor Name</th>
+                                                            <th class="bold-font-weight" >Contract Start Date</th>
+                                                            <th class="bold-font-weight" >Contract End Date</th>
+                                                            <th class="bold-font-weight" >Form Type</th>
                                                             <th class="bold-font-weight" >Status</th>
                                                         </tr>
                                                     </thead>
@@ -272,6 +278,9 @@
                                                             <th class="bold-font-weight" >Resign Date</th>
                                                             <th class="bold-font-weight" >Net Experience</th>
                                                             <th class="bold-font-weight" >Mentor Name</th>
+                                                            <th class="bold-font-weight" >Contract Start Date</th>
+                                                            <th class="bold-font-weight" >Contract End Date</th>
+                                                            <th class="bold-font-weight" >Form Type</th>
                                                             <th class="bold-font-weight" >Status</th>
                                                         </tr>
                                                     </thead>
@@ -901,6 +910,9 @@
                 {data: 'resign_date', name: 'resign_date'},
                 {data: 'net_experience', name: 'net_experience'},
                 {data: 'mentor_name', name: 'mentor_name'},
+                {data: 'contract_start_date', name: 'contract_start_date'},
+                {data: 'contract_end_date', name: 'contract_end_date'},
+                {data: 'form_type', name: 'form_type'},
                 {data: 'status', name: 'status'},
             ],
             "dom": '<"float-left"l><"float-right"f>rt<"bottom float-left"i><"bottom float-right"p><"clear">',
@@ -934,6 +946,9 @@
                 {data: 'resign_date', name: 'resign_date'},
                 {data: 'net_experience', name: 'net_experience'},
                 {data: 'mentor_name', name: 'mentor_name'},
+                {data: 'contract_start_date', name: 'contract_start_date'},
+                {data: 'contract_end_date', name: 'contract_end_date'},
+                {data: 'form_type', name: 'form_type'},
                 {data: 'status', name: 'status'},
             ],
             "dom": '<"float-left"l><"float-right"f>rt<"bottom float-left"i><"bottom float-right"p><"clear">',
@@ -968,6 +983,9 @@
                 {data: 'resign_date', name: 'resign_date'},
                 {data: 'net_experience', name: 'net_experience'},
                 {data: 'mentor_name', name: 'mentor_name'},
+                {data: 'contract_start_date', name: 'contract_start_date'},
+                {data: 'contract_end_date', name: 'contract_end_date'},
+                {data: 'form_type', name: 'form_type'},
                 {data: 'status', name: 'status'},
             ],
             "dom": '<"float-left"l><"float-right"f>rt<"bottom float-left"i><"bottom float-right"p><"clear">',
